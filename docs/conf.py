8743d3d1c3f5355a4fb53849c0720c1fcc9be68a
Hunter <huntrmalm@gmail.com>
2017-07-28 22:45:33 -0400

    Read Description
    Updated docs
    Fixed path to source files in docs\conf.py
    Added game_map parameter to main function to allow loading directly

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../Seven Stories'))
+sys.path.insert(0, os.path.abspath('../SevenStories'))
 
 
 # -- General configuration ------------------------------------------------
